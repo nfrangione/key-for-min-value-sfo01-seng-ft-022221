2f240985431d9e00c704432032ca7ff4cd81bae8
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-12 20:49:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  current_min = 1,000
+  name_hash.each_with_index do |key, value|
+    
+    
 end
